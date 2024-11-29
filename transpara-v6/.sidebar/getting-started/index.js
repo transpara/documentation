7e713bb222fa8b10aaaf08071f21be0db0280e20
Bruna Salgado <bruna@writechoice.io>
2024-11-29 18:00:18 -0300

    docs: adds how to migrate page

--- transpara-v6/.sidebar/getting-started/index.js
+++ transpara-v6/.sidebar/getting-started/index.js
@@ -1,6 +1,12 @@
 module.exports = [
   {
     type: 'doc',
-    id: 'what-is-transpara',
+    id: 'getting-started/what-is-transpara',
+    label: 'What is Transpara?'
+  },
+  {
+    type: 'doc',
+    id: 'getting-started/how-to-migrate-to-transpara',
+    label: 'How to Migrate to Transpara'
   },
 ]
