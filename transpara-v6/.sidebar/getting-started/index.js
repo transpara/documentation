8fc2f683bc3ac966355541798b30c070e8355b23
Bruna Salgado <bruna@writechoice.io>
2024-12-03 11:40:07 -0300

    docs: adding place holder pages

--- transpara-v6/.sidebar/getting-started/index.js
+++ transpara-v6/.sidebar/getting-started/index.js
@@ -6,6 +6,16 @@ module.exports = [
   },
   {
     type: 'doc',
+    id: 'getting-started/transpara-approach',
+    label: 'The Transpara Approach'
+  },
+  {
+    type: 'doc',
+    id: 'getting-started/how-transpara-works',
+    label: 'How Transpara Works'
+  },
+  {
+    type: 'doc',
     id: 'getting-started/how-to-migrate-to-transpara',
     label: 'How to Migrate to Transpara'
   },
